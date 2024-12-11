5fed9dd0a891789f9270d4a57fdc011ef604852d
Iago.R.Prado <101733038+1agoRP@users.noreply.github.com>
2024-12-11 12:48:56 -0300

    Update processa_formulario.php

--- api/processa_formulario.php
+++ api/processa_formulario.php
@@ -1,1 +1,9 @@
-api/processa_formulario.php
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = $_POST['nome'];
+    echo json_encode(['message' => "Olá, $nome!"]);
+} else {
+    http_response_code(405);
+    echo json_encode(['error' => 'Método não permitido']);
+}
+?>
